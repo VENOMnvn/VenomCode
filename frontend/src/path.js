10ad4b14edf093686dcc1374b27596d53bd7ac21
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2023-12-03 20:14:31 +0530

    Build Update

--- frontend/src/path.js
+++ frontend/src/path.js
@@ -1,3 +1,4 @@
 // const path = "http://localhost:5000/";
-const path = "http://192.168.1.4:5000/";
+// const path = "http://192.168.1.4:5000/";
+const path = "https://venomcodebackend.cyclic.app/";
 export default path;
