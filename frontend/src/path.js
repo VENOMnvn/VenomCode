3245bb12636fd543c8bdf78ef87f1a6f6b16fafe
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2023-12-11 08:00:18 +0530

    v.1.6

--- frontend/src/path.js
+++ frontend/src/path.js
@@ -1,6 +1,6 @@
 // const path = "http://localhost:5000/";
-// const path = "https://venomcodebackend.adaptable.app/";
-const path = "http://192.168.43.16:5000/";
+const path = "https://venomcodebackend.adaptable.app/";
+// const path = "http://192.168.43.16:5000/";
 // const path = "https://venomcodebackend.cyclic.app/";
 const version = "v.1.6";
 
