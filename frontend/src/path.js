26b3ea3e17eaab0a49d4b1c40639caee81aee1b9
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2024-03-02 14:10:30 +0530

    v.2.2 FInal youtube link

--- frontend/src/path.js
+++ frontend/src/path.js
@@ -2,8 +2,8 @@
 const path = "https://venomcodebackend.adaptable.app/";
 // const path = "http://192.168.43.16:5000/";
 // const path = "https://venomcodebackend.cyclic.app/";
-const version = "v.2.1";
-const youtubeLogoLink = 'https://www.youtube.com';
+const version = "v.2.2";
+const youtubeLogoLink = 'https://youtu.be/6v0UNrEnmPA?si=jbHqTY65NCQVYbTP';
 
 
 export default path;
