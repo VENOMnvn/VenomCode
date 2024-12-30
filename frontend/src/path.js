ef2805e0131168b60728e858d2bc08479b714223
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2024-12-30 22:38:59 +0530

    Update the new path from render to vercel

--- frontend/src/path.js
+++ frontend/src/path.js
@@ -2,7 +2,7 @@
 // const path = "https://venomcodebackend.onrender.com/";
 // const path = "http://192.168.43.16:5000/";
 // const path = "https://venomcodebackend.cyclic.app/";
-const path = "https://code-fusion-nu.vercel.app/";
+const path = "https://code-fusion-venomnvns-projects.vercel.app/api/";
 const version = "v.2.2";
 const youtubeLogoLink = 'https://youtu.be/6v0UNrEnmPA?si=jbHqTY65NCQVYbTP';
 
